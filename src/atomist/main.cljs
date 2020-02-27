3502b6a2e3f94f80ac6daf6405b7b088ae5dba4a
Jim Clark <slimslenderslacks@gmail.com>
2020-02-27 15:59:38 -0800

    make the schedule talk

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -9,14 +9,22 @@
 (defn middleware [handler]
   (fn [request]
     (go
-     (<! (api/snippet-message request (json/->str (:configurations request)) "application/json" "configuration"))
+     (<! (api/snippet-message request (json/->str (:configuration request)) "application/json" "configuration"))
      (handler request))))
+
+(defn add-slack-source-to-event
+  [handler team-id channel-name]
+  (fn [request]
+    (handler (-> request
+                 (api/default-destination)
+                 (update-in [:destinations 0 :slack] assoc :team {:id team-id} :channel {:name channel-name})))))
 
 (defn command-handler
   "process the request pipeline for any events arriving in this skill"
   [request]
   ((-> (api/finished :message "Command Handler" :success "successfully ran the kitchen sink")
-       (middleware)) request))
+       (middleware)
+       (add-slack-source-to-event "TDDAK8WKT" "kitchen-sink-skill")) request))
 
 (defn ^:export handler
   "handler
@@ -26,3 +34,5 @@
       sendreponse - callback ([obj]) puts an outgoing message on the response topic"
   [data sendreponse]
   (api/make-request data sendreponse command-handler))
+
+(handler #js {:configuration {:crazy "crazy"}} (fn [& args] (println "go" args)))
