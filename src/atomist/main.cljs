60f072e927c01a76d6225d7472d7e7ff7812acd7
Jim Clark <slimslenderslacks@gmail.com>
2020-02-29 16:23:30 -0800

    separate out schedule event handler from command handler

--- src/atomist/main.cljs
+++ src/atomist/main.cljs
@@ -6,17 +6,21 @@
             [atomist.api :as api])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn middleware [handler]
+(defn middleware [handler config-key]
   (fn [request]
     (go
-     (<! (api/snippet-message request (json/->str (:configuration request)) "application/json" "configuration"))
+     (<! (api/snippet-message request (json/->str (config-key request)) "application/json" "configuration"))
      (handler request))))
 
 (defn command-handler
   "process the request pipeline for any events arriving in this skill"
   [request]
   ((-> (api/finished :message "Command Handler" :success "successfully ran the kitchen sink")
-       (middleware)
+       (middleware :configurations)) request))
+
+(defn scheduled-event-handler [request]
+  ((-> (api/finished :message "Scheduled Event Handler" :success "kitchen sink schedule fired")
+       (middleware :configuration)
        (api/add-slack-source-to-event :team-id "TDDAK8WKT" :channel "kitchen-sink-skill")) request))
 
 (defn ^:export handler
@@ -26,4 +30,11 @@
       data - Incoming Request #js object
       sendreponse - callback ([obj]) puts an outgoing message on the response topic"
   [data sendreponse]
-  (api/make-request data sendreponse command-handler))
+  (api/make-request
+   data sendreponse
+   (fn [request]
+     (cond
+       (= "KitchenSink" (:command request))
+       (command-handler request)
+       :else
+       (scheduled-event-handler request)))))
