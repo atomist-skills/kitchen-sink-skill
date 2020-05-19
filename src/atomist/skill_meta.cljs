4675af47cf19c062f2287fd2bd9ab23e18b4dfc5
Jim Clark <slimslenderslacks@gmail.com>
2020-05-18 17:25:08 -0700

    Bump kitchen-sink-skill to 0.1.80 and spit metadata

--- src/atomist/skill_meta.cljs
+++ src/atomist/skill_meta.cljs
@@ -3,6 +3,6 @@
 
 (def module-name "kitchen-sink-skill")
 (def path ".")
-(def version "0.1.79")
-(def tag "0.1.79")
-(def generated-at "2020-05-18T23:54:57Z")
+(def version "0.1.80")
+(def tag "0.1.80")
+(def generated-at "2020-05-19T00:25:08Z")
